fb5ea97a76597a2acc24798012a2219f61266dea
Tony Wu <tonywu_18@hotmail.com>
2021-06-10 01:18:51 +1000

    add server skeleton

---
+++ server/src/main/java/com/example/todosprintboot/TodoSprintbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.todosprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSprintbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSprintbootApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/todosprintboot/TodoSprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todosprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
