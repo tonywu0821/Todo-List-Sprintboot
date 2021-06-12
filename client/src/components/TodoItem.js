4ea52713c990c0d2377efef7c6c5485983d0958d
Tony Wu <tonywu_18@hotmail.com>
2021-06-12 13:00:12 +1000

    Add readme

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -4,21 +4,24 @@ import EditTodo from "./EditTodo";
 const TodoItem = ({todo, updateTodo, deleteTodo}) => {  
   return ( 
     <div 
-      className={todo.done ? "todo-row complete": "todo-row"} 
+      className={todo.done ? "complete": "todo-row"} 
     >
       <div className={todo.done ? "complete-text": "" } onClick={() => updateTodo(todo.id, todo.content, !todo.done) }>
         {todo.content}
       </div>
       <div>
-        <EditTodo todo ={todo} updateTodo = {updateTodo}/>
-        <button
-          className="btn btn-danger"
-          onClick={() => deleteTodo(todo.id)}
-        >
-          Delete
-        </button>
+      <EditTodo todo ={todo} updateTodo = {updateTodo}/>
+      <button
+        className="btn btn-danger"
+        onClick={() => deleteTodo(todo.id)}
+      >
+        Delete
+      </button>
       </div>
+  
     </div>  
+    
+    
   )
 }
 
